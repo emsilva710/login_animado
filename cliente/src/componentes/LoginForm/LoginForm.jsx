c99c35682f4ff721a2865d688a7cfa1905581ca2
emsilva710 <eswardomoises@gmail.com>
2024-05-06 10:16:05 -0400

    commit4

--- cliente/src/componentes/LoginForm/LoginForm.jsx
+++ cliente/src/componentes/LoginForm/LoginForm.jsx
@@ -1,44 +1,43 @@
 import React from 'react';
 import './LoginForm.css';
-import { PiUserFill } from "react-icons/pi";
-import { IoLockClosed } from "react-icons/io5";
-
-
+import { PiUserFill } from 'react-icons/pi';
+import { IoLockClosed } from 'react-icons/io5';
 
 const LoginForm = () => {
-  return (
-    <body>
-        <div className='blob'></div>
-        <div className='logo'> </div>
-        <div className='wrapper'>
-        <form action="">
-       
-          
-            <div className='input-box'>
-                <PiUserFill className='icon' />
-                <input type="text" required/>
-                <label>Usuario</label>
+    return (
+        <div className="login-form-container">
+            <div className="blob"></div>
+            <div className="logo"></div>
+            <div className="wrapper">
+
+                <form action="">
+                    <div className="input-box">
+                        <PiUserFill className="icon" />
+                        <input type="text" required />
+                        <label>Usuario</label>
+                    </div>
+                    <div className="input-box">
+                        <IoLockClosed className="icon" />
+                        <input type="password" required />
+                        <label>Contraseña</label>
+                    </div>
+                    <div className="remember-forgot">
+                        <label>
+                            <input type="checkbox" />
+                            Mostrar contraseña
+                        </label>
+                        <a href="#">Has olvidado tu contraseña?</a>
+                    </div>
+                    <button type="submit">Acceder</button>
+                    <div className="register-link">
+                        <p>Cooperativa 8 de Marzo Ltda.®</p>
+                    </div>
+                </form>
             </div>
-            <div className='input-box'>
-                <IoLockClosed className='icon' />
-                <input type="password" required/>
-                <label>Contraseña</label>
-            </div>
-            <div className='remember-forgot'>
-                <label><input type="checkbox" />
-                     Mostrar contraseña
-                </label>
-                <a href="#">Has olvidado tu contraseña?</a>
-            </div>
-            <button type='submit'>Acceder</button>
-            <div className="register-link">
-                <p>Cooperativa 8 de Marzo Ltda.</p>
-            </div>
-        </form>
-    </div>
-    </body>
-    
-  );
+        </div>
+    );
 };
 
-export default LoginForm
+export default LoginForm;
+
+
