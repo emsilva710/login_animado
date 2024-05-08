54fd1d5b8c50f41ac0bfa4d95daccae645477892
emsilva710 <eswardomoises@gmail.com>
2024-05-08 13:47:57 -0400

    commit6

--- cliente/src/componentes/LoginForm/LoginForm.jsx
+++ cliente/src/componentes/LoginForm/LoginForm.jsx
@@ -5,36 +5,40 @@ import { IoLockClosed } from 'react-icons/io5';
 
 const LoginForm = () => {
     return (
-        <div className="login-form-container">
-            <div className="blob"></div>
-            <div className="logo"></div>
-            <div className="wrapper">
 
-                <form action="">
-                    <div className="input-box">
-                        <PiUserFill className="icon" />
-                        <input type="text" required />
-                        <label>Usuario</label>
-                    </div>
-                    <div className="input-box">
-                        <IoLockClosed className="icon" />
-                        <input type="password" required />
-                        <label>Contraseña</label>
-                    </div>
-                    <div className="remember-forgot">
-                        <label>
-                            <input type="checkbox" />
-                            Mostrar contraseña
-                        </label>
-                        <a href="#">Has olvidado tu contraseña?</a>
-                    </div>
-                    <button type="submit">Acceder</button>
-                    <div className="register-link">
-                        <p>Cooperativa 8 de Marzo Ltda.®</p>
-                    </div>
-                </form>
+        <div className='container'>
+            <div className="login-form-container">
+                <div className="blob"></div>
+                <div className="logo"></div>
+                <div className="wrapper">
+
+                    <form action="">
+                        <div className="input-box">
+                            <PiUserFill className="icon" />
+                            <input type="text" required />
+                            <label>Usuario</label>
+                        </div>
+                        <div className="input-box">
+                            <IoLockClosed className="icon" />
+                            <input type="password" required />
+                            <label>Contraseña</label>
+                        </div>
+                        <div className="remember-forgot">
+                            <label>
+                                <input type="checkbox" />
+                                Mostrar contraseña
+                            </label>
+                            <a href="#">Has olvidado tu contraseña?</a>
+                        </div>
+                        <button type="submit">Acceder</button>
+                        <div className="register-link">
+                            <p>Cooperativa 8 de Marzo Ltda.®</p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
+
     );
 };
 
